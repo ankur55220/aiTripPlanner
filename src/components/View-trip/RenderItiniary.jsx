b7bb9e1af55cb74e3e945a1fd595ee25c6438830
ankur4591 <ankur.verma@repolite.com>
2025-03-20 23:13:51 +0530

    added google map redirection to places

--- src/components/View-trip/RenderItiniary.jsx
+++ src/components/View-trip/RenderItiniary.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 function RenderItiniary({data}) {
+  const openInGoogleMaps = (hotel) => {
+       
+    // Fall back to search using name and address for better accuracy
+    const searchQuery = encodeURIComponent(`${hotel.name} ${hotel.location.address}`);
+    window.open(`https://www.google.com/maps/search/?api=1&query=${searchQuery}`, '_blank');
+
+};
   return (
-    <div className='p-4 rounded-lg shadow-md'>
+    <div className='p-4 rounded-lg shadow-md cursor-pointer' onClick={()=>openInGoogleMaps(data)}>
         <div className='w-full h-[200px] overflow-hidden'>
             <img src={data?.image_url || '/placeholder.jpg'} alt="" className='w-full h-full object-cover' />
         </div>
